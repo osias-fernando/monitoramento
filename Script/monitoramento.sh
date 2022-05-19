ca31eb1fdfaee58ad408bcc8a39f75ecf231cb8b
osias-fernando <osias26@hotmail.com>
2022-05-19 14:25:36 -0300

    Grafana

--- Script/monitoramento.sh
+++ Script/monitoramento.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo ""
 cd /opt/
 echo "---------- Instalando o Prometheus ----------"
 sudo apt update
@@ -40,3 +41,19 @@ WantedBy=multi-user.target" > /etc/systemd/system/prometheus.service
 
 sudo systemctl start prometheus
 sudo systemctl enable prometheus
+echo ""
+
+sleep 5
+
+echo "---------- Instalando o Grafana ----------"
+echo ""
+sudo apt-get install -y apt-transport-https
+sudo apt-get install -y software-properties-common wget
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+sudo apt-get update -y
+sudo apt-get install grafana -y
+sudo systemctl daemon-reload
+sudo systemctl start grafana-server
+sudo systemctl status grafana-server
+sudo systemctl enable grafana-server.service
